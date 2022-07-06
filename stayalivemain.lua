4274ea3bd03433a4318ee64632a3705711662109
SkeidoxDev <77990035+SkeidoxDev@users.noreply.github.com>
2022-07-06 10:59:53 -0400

    Update stayalivemain.lua

--- stayalivemain.lua
+++ stayalivemain.lua
@@ -1,3 +1,4 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SkeidoxDev/36paidcracked/main/36paid.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/SkeidoxDev/36paidcracked/main/infyield.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/main/spychat.lua"))()
 
